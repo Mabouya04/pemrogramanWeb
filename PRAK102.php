30e60580075ad1072e091a0bfbbf837e5103d8dc
Mabouya04 <62530808+Mabouya04@users.noreply.github.com>
2022-03-17 16:17:01 +0800

    Add files via upload

---
+++ PRAK102.php
@@ -0,0 +1,11 @@
+<?php
+$tinggi = 5.4;
+$panjang = 8.9;
+$lebar = 14.7;
+
+$alas = $panjang*$lebar;
+$volume = $alas*$tinggi/3;
+number_format($alas, );
+
+echo round($volume,3)." m3"
+?>
---
+++ PRAK104.php
@@ -0,0 +1,25 @@
+<html>
+    <?php
+    $daftar = array("Samsung Galaxy S22","Samsung Galaxy S22 +", "Samsung Galaxy A03", "Samsung Galaxy Xcover 5");
+    ?>   
+<body>
+    <table border="1">
+   <tr>
+    <th>Daftar Smartphone Samsung</th>
+   </tr>
+   <tr>
+    <td><?php echo $daftar[0]?></td>
+   </tr>
+   <tr>
+    <td><?php echo $daftar[1]?></td>
+   </tr>
+   <tr>
+    <td><?php echo $daftar[2]?></td>
+   </tr>
+   <tr>
+    <td><?php echo $daftar[3]?></td>
+   </tr>
+    </table>
+</body>
+</html>
+
